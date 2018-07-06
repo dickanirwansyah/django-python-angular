9376a83bfab7ecccb1c200179660427c24ed5940
dickanirwansyah <dickanirwansyah@gmail.com>
2018-07-06 14:53:48 +0700

    image

--- imageupload_frontend/static/js/app.js
+++ imageupload_frontend/static/js/app.js
@@ -4,8 +4,9 @@ myApp.config(function($resourceProvider){
     $resourceProvider.defaults.stripTrailingSlashes = false;
 });
 
-myApp.controller('MainCtrl', function($scope){
+myApp.controller('MainCtrl', function($scope, Images){
 
    console.log("Congratulations You Are In MainCtrl!");
 
+    $scope.images = Images.query()
 });
