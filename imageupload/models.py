2fbc4eba9ab8079abb30cf58bd2769a80bd9ccc7
dickanirwansyah <dickanirwansyah@gmail.com>
2018-07-06 13:19:48 +0700

    scramble uuid images file name

--- imageupload/models.py
+++ imageupload/models.py
@@ -1,4 +1,12 @@
+import uuid
 from django.db import models
 
+
+#penamaan file unique image dengan UUID4
+def scramble_uploaded_image_filename(instance, filename):
+    extension = filename.split(".")[-1]
+    return "{}.{}".format(uuid.uuid4(), extension)
+
+
 class UploadImage(models.Model):
-    image = models.ImageField("Upload Image") #stores the images
+    image = models.ImageField("Upload Image", upload_to=scramble_uploaded_image_filename) #stores the images
